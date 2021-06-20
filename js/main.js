7017a40e6fa58f308ee53087aaa3306515770281
Tanasenko Konstanrin <70750471+Tanasenko@users.noreply.github.com>
2021-06-20 22:27:21 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const miniMenu = document.querySelector('.mini-menu');
+
+hamburger.addEventListener('click', e =>{
+    if ( miniMenu.style.display == 'none') {
+        miniMenu.style.setProperty('display', 'initial');
+    } else miniMenu.style.display = 'none';
+});
