4b1e977d0c53ceba28ad14b63d0938ed7095c552
Tanasenko Konstanrin <70750471+Tanasenko@users.noreply.github.com>
2021-06-29 16:43:25 +0400

    Add files via upload

--- js/accardion.js
+++ js/accardion.js
@@ -1,9 +1,47 @@
 $(document).ready(() => {
 
-    $('.team__title').on('click', e => {
-        $(e.currentTarget).toggleClass('team__title--active');
-        $(e.currentTarget).next().toggleClass('accordion--active');
-    })
+    //$('.team__title').on('click', e => {
+        //$(e.currentTarget).toggleClass('team__title--active');
+        //if($('.team__title--active') == true){
+        //    $(document).getElementById('.team__item').style.marginBottom = `${385}px`;
+       // }
+       // $(e.currentTarget).next().toggleClass('accordion--active');
+    //})
+
+    const openItem = item => {
+        const container = item.closest('.team__item');
+        const contentBlock = container.find('.accordion');
+        const textBlock = contentBlock.find('.accordion__iner');
+        const reqHeight = textBlock.height();
+
+        container.addClass('active');
+        contentBlock.height(reqHeight);
+    }
+
+    const closeEveryItem = container => {
+        const items = container.find('.accordion');
+        const itemContainer = container.find('.team__item');
+
+        itemContainer.removeClass('active');
+        items.height(0);
+    }
+
+    $('.team__title').on('click', e =>{
+        const $this = $(e.currentTarget);
+        const container = $this.closest('.team__list');
+        const elemConteiner = $this.closest('.team__item');
+        $this.toggleClass('team__title--active');
+
+        if(elemConteiner.hasClass('active')){
+        closeEveryItem(container);
+        } else {
+        closeEveryItem(container);
+        openItem($this);
+        
+        }
+
+        
+    });
 
 
 });
